79b63d4eff2c2cb00364dbf99b8653db9f1957e3
Tom Elvidge <tee504@york.ac.uk>
2019-12-27 13:47:23 +0000

    Search page styling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./style.css";
 
 function App() {
   return (
-    <div>
-      <h1>What's in?</h1>
+    <div className="search-div">
+      <h1 className="search-text">What's in</h1>
+      <input className="search-text search-text-input" type="text" />
+      <h1 className="search-text">?</h1>
     </div>
   );
 }
